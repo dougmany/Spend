6ecc1a9238f0732c2a07c4321c074327e9608cfa
Doug Meeker <wog400@hotmail.com>
2026-02-20 16:20:29 -0800

    Update to dotnet 10.

--- Spend/Startup.cs
+++ Spend/Startup.cs
@@ -2,16 +2,13 @@
 using Dapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Spend.Data;
 using Spend.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Spend
 {
