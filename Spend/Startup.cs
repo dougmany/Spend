0fe0b128a9c470080d280d6190047346d94fd00e
Doug Meeker <wog400@hotmail.com>
2026-03-17 19:42:24 -0700

    use basepath.

--- Spend/Startup.cs
+++ Spend/Startup.cs
@@ -42,6 +42,8 @@ namespace Spend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
+            app.UsePathBase("/spend");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
