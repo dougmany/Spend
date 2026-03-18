fd8352d98f023bc163edbc754af93887ace88ea7
Doug Meeker <wog400@hotmail.com>
2026-03-17 19:48:50 -0700

    Fix folder path.

--- Spend/Startup.cs
+++ Spend/Startup.cs
@@ -42,7 +42,11 @@ namespace Spend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
-            app.UsePathBase("/spend");
+            app.Use((context, next) =>
+            {
+                context.Request.PathBase = "/spend";
+                return next();
+            });
 
             if (env.IsDevelopment())
             {
